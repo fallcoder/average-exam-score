4197224374b49f1b4d0e1961f0986ce664e8fb15
fallcoder <fallcoder@users.noreply.github.com>
2024-09-27 14:57:17 +0000

    le fichier principal pour exécuter le programme

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// importation du module readline pour lire les entrées utilisateur dans la console
+import { createInterface } from 'readline';
+import { mainMenu } from './js/menu.js';
+
+// création de l'interface readline
+const r1 = createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// lancement du programme avec le menu principal
+mainMenu(r1)
