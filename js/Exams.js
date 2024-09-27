efb7c37bda5fe3791fa3e95d4c97f0afa827f0de
fallcoder <fallcoder@users.noreply.github.com>
2024-09-27 12:01:41 +0000

    déplacement de Exams.js dans le dossier js. Ce fichier contient la logique de calcul de la moyenne

---
+++ js/Exams.js
@@ -0,0 +1,11 @@
+// définition de la class Exams avec une méthode static pour calculer la moyenne
+class Exams {
+  static average(notes) {
+    const sum = notes.reduce((acc, note) => acc + note, 0);
+    const avg = sum / notes.length;
+    return avg.toFixed(2); // renvoie la moy avec 2 décimales
+  }
+}
+
+// exporter directement la méthode average
+export const average = Exams.average;
